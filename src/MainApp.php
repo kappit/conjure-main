da9afd020d19cdec84790cd0057f8e22bba37362
Kappit <kasper.h90@outlook.dk>
2021-01-08 21:33:21 +0100

    Added MainApp.

---
+++ src/MainApp.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conjure\Main;
+
+
+/**
+ * Class MainApp
+ * @package Conjure\Main
+ */
+class MainApp
+{
+    
+    public static function main(): void
+    {
+
+    }
+
+}
